96a9c9f0af8cc95e8e476d662dfa70f21cd4578e
Nabeel Ahmed <fussdeek@gmail.com>
2026-09-22 10:40:16 +0500

    Create return.py

---
+++ python/return.py
@@ -0,0 +1,16 @@
+
+"""Keyword arguments
+Call functions using parameter names for clarity:
+"""
+
+def print_nums(a, b):
+  print(a + b)
+
+  print_nums(a=5, b=10)
+
+def num_return(a, b):
+  return a + b
+
+  result = num_return(a=5, b=10)
+
+  result + 344
