8420eac220967998c8e160a90c4acb86a0451b4b
Nabeel Ahmed <fussdeek@gmail.com>
2026-09-25 18:46:15 +0500

    Update API_in_Python.py

--- API_in_Python.py
+++ API_in_Python.py
@@ -1,4 +1,6 @@
 import requests
+from datetime import datetime, timedelta
+
 
 # We need coordinates to get weather data
 latitude = 48.85   # Paris latitude
@@ -11,4 +13,68 @@ url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={lo
 response = requests.get(url)
 data = response.json()
 
+print(data["current"]["temperature_2m"])  # Print the current temperature in Celsius
+
+
+
+
+
+def get_weather(latitude, longitude):
+    response = requests.get(f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current=temperature_2m,wind_speed_10m")
+    data = response.json()
+    return data['current']['temperature_2m']
+
+# Get temperature for different cities
+paris_temp = get_weather(48.85, 2.35)
+london_temp = get_weather(51.50, -0.12)
+tokyo_temp = get_weather(35.68, 139.69)
+
+print(f"Paris: {paris_temp}°C")
+print(f"London: {london_temp}°C")
+print(f"Lahore: {tokyo_temp}°C")
+
+
+"""Get 7 days of weather
+The Open-Meteo API can give us historical data:"""
+
+from datetime import datetime, timedelta
+
+today = datetime.now()
+week_ago = today - timedelta(days=7)
+
+start_date = week_ago.strftime("%Y-%m-%d")
+end_date = today.strftime("%Y-%m-%d")
+
+url = (
+    "https://api.open-meteo.com/v1/forecast"
+    f"?latitude=48.85&longitude=2.35"
+    f"&start_date={start_date}&end_date={end_date}"
+    "&daily=temperature_2m_max,temperature_2m_min"
+)
+
+response = requests.get(url)
+response.raise_for_status()
+data = response.json()
+
 print(data)
+
+
+
+
+"""WORKING WITH PANDAS"""
+
+import pandas as pd
+# Extract the daily data
+daily_data = data['daily']
+
+# Create a DataFrame
+df = pd.DataFrame({
+    'date    ': daily_data['time'],
+    'max_temp': daily_data['temperature_2m_max'],
+    'min_temp': daily_data['temperature_2m_min']
+})
+
+# Convert date strings to datetime
+df['date'] = pd.to_datetime(df['date'])
+
+print(df)
