d4d548754a64e4d55103038d5f33422578cbea31
Nabeel Ahmed <fussdeek@gmail.com>
2026-09-26 19:05:42 +0500

    Update lesson1.py

--- python/api_data/lesson1.py
+++ python/api_data/lesson1.py
@@ -1,3 +1,12 @@
 import requests
 import pandas as pd
 import matplotlib.pyplot as plt
+
+
+
+url = "https://api.github.com"
+
+response = requests.get(url)
+
+print(response.status_code)
+print(response.json())
