24f6eb27469aa43058fee6429ec53c2a526e4835
Nabeel Ahmed <fussdeek@gmail.com>
2026-09-26 19:13:34 +0500

    Update lesson1.py

--- python/api_data/lesson1.py
+++ python/api_data/lesson1.py
@@ -2,13 +2,13 @@ import requests
 import pandas as pd
 import matplotlib.pyplot as plt
 
-print("Everything works!")
-
-
-
-url = "https://api.github.com"
+url = "https://api.github.com/repos/python/cpython/issues?per_page=10"
 
 response = requests.get(url)
 
 print(response.status_code)
-print(response.json())
+
+data = response.json()
+
+print(type(data))
+print(len(data))
